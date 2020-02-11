2ff42ad66538741d2aca06bb2646804402e766a5
Alistair Smith <aabbccsmith@users.noreply.github.com>
2020-02-11 18:03:00 +0000

    fixxxx

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const createWindow = () => {
     width: 900,
   });
 
-  mainWindow.loadURL(path.join(__dirname, "renderer", "index.html"));
+  mainWindow.loadFile(path.join(__dirname, "renderer", "index.html"));
 };
 
 ipc.on('quit', app.quit);
