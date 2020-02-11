9c636883c0dce8cebd82c6854571223c40109a47
Alistair Smith <aabbccsmith@users.noreply.github.com>
2020-02-11 17:59:10 +0000

    ugh

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-/**
- * Your main application goes in this file.
- */
+const { app, BrowserWindow, ipcMain: ipc } = require('electron');
+const path = require('path');
+let mainWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    height: 550,
+    width: 900,
+  });
+
+  mainWindow.loadURL(path.join(__dirname, "renderer", "index.html"));
+};
+
+ipc.on('quit', app.quit);
+app.on('ready', createWindow);
+app.on('window-all-closed', app.quit);
+app.on('activate', () => BrowserWindow.getAllWindows().length === 0 && createWindow());
